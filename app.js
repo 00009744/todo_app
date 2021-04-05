da51671eb13bfd7ba8a9216dd33d15be3d071a05
00009744 <wiut00009744@gmail.com>
2021-04-05 19:34:09 +0500

    homepage

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const app = express()
 
-
 app.set('view engine', 'pug')
+
+
 app.use('/static', express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('maintodo')
+app.get('/create', (req, res) => {
+    res.render('create')
 })
 
+// localhost:5000
+app.get('/', (req, res) => {
+    res.render('mainpage')
+})
 
+app.listen(5000, err => {
+    if (err) console.log(err)
 
-const listener = app.listen(9000, () => {
-    console.log(`App is running on port  http://localhost:9000`);
-  });
+    console.log('App is running on port 5000')
+})
