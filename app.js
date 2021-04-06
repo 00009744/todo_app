05d33851444e5519bceec280d1f5c01b6633c97d
00009744 <wiut00009744@gmail.com>
2021-04-06 13:41:31 +0500

    showing saved  data

--- app.js
+++ app.js
@@ -1,28 +1,67 @@
 const express = require('express')
 const app = express()
 
+const fs = require('fs')
+
 app.set('view engine', 'pug')
 
 
 app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
+
+app.post('/create', (req, res) => {
+    const name = req.body.name
+    const description = req.body.description
+
+    if (name.trim() === '' && description.trim() === '') {
+        res.render('create', { error: true })
+    } else {
+        fs.readFile('./db/tasks.json', (err, db) => {
+            if (err) throw err
+
+            const tasks = JSON.parse(db)
+
+            tasks.push({
+                id: id (),
+                name: name,
+                description: description,
+            })
+            fs.writeFile('./db/tasks.json', JSON.stringify(tasks), err => {
+                if (err) throw err 
+                res.render('create', { success: true })
+            })
+        })
+    }
+})
+
+function id (){
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
 
 app.get('/create', (req, res) => {
     res.render('create')
 })
 
 
-const tasks = ['Some', 'Some 2']
-
 app.get('/tasks', (req, res) => {
-    res.render('tasks', { tasks: tasks })
+
+    fs.readFile('./db/tasks.json', (err, db) => {
+        if (err) throw err
+
+        const tasks = JSON.parse(db)
+        res.render('tasks', { tasks: tasks })
+    })
 })
+
+
 
 app.get('/tasks/detail', (req, res) => {
     res.render('detail')
 })
 
 // localhost:5000
-app.get('/mainpage', (req, res) => {
+app.get('/', (req, res) => {
     res.render('mainpage')
 })
 
